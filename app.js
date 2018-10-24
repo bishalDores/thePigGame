3e43681a5d875040ff33d79bd3e19399314a32db
bishalpd <bishal@gmail.com>
2018-10-24 13:59:45 +0600

    score limit updated

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         //update the UI
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
         //check if the player won
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = "Winner!";
             document.querySelector('.dice').style.display = 'none';
             document.querySelector(".player-" + activePlayer + "-panel").classList.add('winner');
